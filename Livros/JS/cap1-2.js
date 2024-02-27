0df03a52143ebc6d691d0b7342a51c074b95698a
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-27 13:15:15 -0300

    update

---
+++ Livros/JS/cap1-2.js
@@ -0,0 +1,11 @@
+let a = []; //array vazio
+
+console.log(a);
+
+a.push(1, 2, 3); //acrescenta no array
+
+console.log(a);
+
+a.reverse(); //inverte a ordem do array
+
+console.log(a);
---
+++ Livros/JS/cap1-3.js
@@ -0,0 +1,11 @@
+points.dist = function() {
+    let p1 = this[0];
+    let p2 = this[1];
+
+    let a = p2.x-p1.x;
+    let b = p2.y-p1.y;
+
+    return Math.sqrt(a*a + b*b);
+}
+
+points.dist(1, 2);
