3f24efbd7ce6989c10a6d7a29ef5776ed964291c
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-04-08 12:17:09 -0300

    teste

---
+++ Analise-de-Sistemas/debugger/debugger/Program.cs
@@ -0,0 +1,20 @@
+﻿# include<stdio.h>
+
+
+int main()
+{
+    int n, first = 0, second = 1, next, c;
+
+    printf("Digite o numero de termos desejados\n");
+    scanf("%d", &n)
+}
+
+unsafe void scanf(string v1, int* v2)
+{
+    throw new NotImplementedException();
+}
+
+void printf(string v)
+{
+    throw new NotImplementedException();
+}
